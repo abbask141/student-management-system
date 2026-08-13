67f08ed78b7f7be1e2a6ce3a328a724198952cba
abbask141 <abbas033378@gmail.com>
2026-08-13 21:27:00 +0500

    Fix database connection for Neon

--- database.py
+++ database.py
@@ -1,17 +1,11 @@
 import os
-import mysql.connector
-from dotenv import load_dotenv
-
-load_dotenv()
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 
 def get_db_connection():
-
-    connection = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+    connection = psycopg2.connect(
+        os.getenv("DATABASE_URL")
     )
 
     return connection
